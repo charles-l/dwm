ee862404f19dd602508b09a704468ab96f9535bc
charlie <chuckiels2011@gmail.com>
2016-04-19 20:50:52 -0400

    updated resizing code slightly

--- dwm.c
+++ dwm.c
@@ -487,11 +487,8 @@ moveresize(const Arg *arg) {
 	/* only floating windows can be moved */
 	Client *c;
 	c = selmon->sel;
-	int x, y, w, h, nx, ny, nw, nh, ox, oy, ow, oh;
+	int x, y, w, h, nx, ny, nw, nh;
 	char xAbs, yAbs, wAbs, hAbs;
-	int msx, msy, dx, dy, nmx, nmy;
-	unsigned int dui;
-	Window dummy;
 
 	if (!c || !arg)
 		return;
@@ -528,22 +525,12 @@ moveresize(const Arg *arg) {
 			ny = y;
 	}
 
-	ox = c->x;
-	oy = c->y;
-	ow = c->w;
-	oh = c->h;
-
 	XRaiseWindow(dpy, c->win);
-	Bool xqp = XQueryPointer(dpy, root, &dummy, &dummy, &msx, &msy, &dx, &dy, &dui);
 	resize(c, nx, ny, nw, nh, True);
 
+
 	/* move cursor along with the window to avoid problems caused by the sloppy focus */
-	if (xqp && ox <= msx && (ox + ow) >= msx && oy <= msy && (oy + oh) >= msy)
-	{
-		nmx = c->x - ox + c->w - ow;
-		nmy = c->y - oy + c->h - oh;
-		XWarpPointer(dpy, None, None, 0, 0, 0, 0, nmx, nmy);
-	}
+	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w + c->bw - 1, c->h + c->bw - 1);
 }
 
 void
