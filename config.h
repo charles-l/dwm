ba85a4246cab8daeecf1e87ff360ea57c429b72f
charlie <chuckiels2011@gmail.com>
2016-04-22 20:24:52 -0400

    patched in maximize

--- config.h
+++ config.h
@@ -18,6 +18,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
+#include "maximize.c"
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5"};
 
@@ -62,38 +64,40 @@ static const char *raisevolumecmd[] = {"volume", "+"};
 static const char *lowervolumecmd[] = {"volume", "-"};
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_j, 	   moveresize,     {.v  =  "0x 80y 0w 0h"}},
-	{ MODKEY|ControlMask,           XK_k,  	   moveresize,     {.v  =  "0x -80y 0w 0h"}},
-	{ MODKEY|ControlMask,           XK_l,  	   moveresize,     {.v  =  "80x 0y 0w 0h"}},
-	{ MODKEY|ControlMask,           XK_h,  	   moveresize,     {.v  =  "-80x 0y 0w 0h"}},
-	{ MODKEY|ControlMask|ShiftMask, XK_j,  	   moveresize,     {.v  =  "0x 0y 0w 80h"}},
-	{ MODKEY|ControlMask|ShiftMask, XK_k,  	   moveresize,     {.v  =  "0x 0y 0w -80h"}},
-	{ MODKEY|ControlMask|ShiftMask, XK_l,  	   moveresize,     {.v  =  "0x 0y 80w 0h"}},
-	{ MODKEY|ControlMask|ShiftMask, XK_h,  	   moveresize,     {.v  =  "0x 0y -80w 0h"}},
+	/* modifier                     key        function            argument */
+	{ MODKEY,                       XK_p,      spawn,              {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,              {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,          {0} },
+	{ MODKEY,                       XK_j,      focusstack,         {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,         {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,         {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_h,      setmfact,           {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_l,      setmfact,           {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,               {0} },
+	{ MODKEY,                       XK_Tab,    view,               {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,         {0} },
+	{ MODKEY,                       XK_t,      setlayout,          {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,          {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,          {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,          {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating,     {0} },
+	{ MODKEY,                       XK_0,      view,               {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,                {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,           {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,           {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,             {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,             {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_j, 	   moveresize,         {.v  =  "0x 80y 0w 0h"}},
+	{ MODKEY|ControlMask,           XK_k,  	   moveresize,         {.v  =  "0x -80y 0w 0h"}},
+	{ MODKEY|ControlMask,           XK_l,  	   moveresize,         {.v  =  "80x 0y 0w 0h"}},
+	{ MODKEY|ControlMask,           XK_h,  	   moveresize,         {.v  =  "-80x 0y 0w 0h"}},
+	{ MODKEY|ControlMask|ShiftMask, XK_j,  	   moveresize,         {.v  =  "0x 0y 0w 80h"}},
+	{ MODKEY|ControlMask|ShiftMask, XK_k,  	   moveresize,         {.v  =  "0x 0y 0w -80h"}},
+	{ MODKEY|ControlMask|ShiftMask, XK_l,  	   moveresize,         {.v  =  "0x 0y 80w 0h"}},
+	{ MODKEY|ControlMask|ShiftMask, XK_h,  	   moveresize,         {.v  =  "0x 0y -80w 0h"}},
+	{ MODKEY|ControlMask, 			XK_m,      toggleverticalmax,   NULL },
+	{ MODKEY|ControlMask|ShiftMask,	XK_m,      togglehorizontalmax, NULL },
 	{ 0, XF86XK_AudioRaiseVolume          , spawn,          {.v = raisevolumecmd } },
 	{ 0, XF86XK_AudioLowerVolume          , spawn,          {.v = lowervolumecmd } },
 	TAGKEYS(                        XK_1,                      0)
--- dwm.c
+++ dwm.c
@@ -97,7 +97,7 @@ struct Client {
 	int basew, baseh, incw, inch, maxw, maxh, minw, minh;
 	int bw, oldbw;
 	unsigned int tags;
-	int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen, isterminal, noswallow;
+	int ismax, wasfloating, isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen, isterminal, noswallow;
 	pid_t pid;
 	Client *next;
 	Client *snext;
@@ -1287,6 +1287,8 @@ manage(Window w, XWindowAttributes *wa)
 	updatewmhints(c);
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
+	c->wasfloating = 0;
+	c->ismax = 0;
 	if (!c->isfloating)
 		c->isfloating = c->oldstate = trans != None || c->isfixed;
 	if (c->isfloating)
